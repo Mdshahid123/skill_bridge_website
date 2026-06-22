f7ec3c22471183723858625fd13bb344cefc47e2
md shahid <110709082+Mdshahid123@users.noreply.github.com>
2026-06-22 16:18:33 +0530

    Update email transporter configuration to use environment variables

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,13 +2,17 @@
 const nodemailer = require('nodemailer');
 
 //Create transporter
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // or 'outlook', 'yahoo', etc.
+    host: 'smtp.hostinger.com',
+    port: 465,
+    secure: true,
     auth: {
-        user: 'shahiddelhi989@gmail.com',     // Your email address
-        pass: 'orvd swqd lpel wwxv'         // App password (not regular password)
+        user: process.env.EMAIL_USER,  // Your email address
+        pass: process.env.EMAIL_PASS
     }
 });
+
 
 // For Gmail, you need to:
 // 1. Enable 2-factor authentication
